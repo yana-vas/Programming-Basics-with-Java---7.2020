b0129971791419f96f5c1c3ae453d82daa1eb7e5
Yana Vasileva <58726634+YanaVasileva07@users.noreply.github.com>
2020-07-16 17:17:22 +0300

    Add files via upload

---
+++ First Steps In Coding - Lab/Greeting_byName.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Greeting_byName {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+
+
+    }
+}
